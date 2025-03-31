c1c51984ca0ba5ca6192474b5afb79fe8fbc5afc
Akhil1sharma <168325399+Akhil1sharma@users.noreply.github.com>
2025-03-31 20:42:41 +0000

    update database file

--- Cook-Book App/backend/config/connectionDb.js
+++ Cook-Book App/backend/config/connectionDb.js
@@ -1,8 +1,11 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
-const connectDb=async()=>{
-    await mongoose.connect(process.env.CONNECTION_STRING)
-    .then(()=>console.log("connected..."))
-}
+// Connect to MongoDB
+const connectDb = async () => {
+    await mongoose.connect(process.env.CONNECTION_STRING) // Use env connection string
+    .then(() => console.log("connected...")) // Log success message
+    .catch((error) => console.error("Connection failed:", error)); // Handle errors
+};
 
-module.exports=connectDb
+// Export function
+module.exports = connectDb
