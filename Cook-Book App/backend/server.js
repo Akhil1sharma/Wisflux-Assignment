319a114f61af646fac4e787c54704ddda512819d
Akhil1sharma <168325399+Akhil1sharma@users.noreply.github.com>
2025-04-03 15:06:56 +0530

    Update server.js

--- Cook-Book App/backend/server.js
+++ Cook-Book App/backend/server.js
@@ -8,15 +8,11 @@ const PORT=process.env.PORT || 3000
 connectDb()
 
 app.use(express.json())
-app.use(cors());
+app.use(cors())
 app.use(express.static("public"))
 
 app.use("/",require("./routes/user"))
 app.use("/recipe",require("./routes/recipe"))
-
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
 
 app.listen(PORT,(err)=>{
     console.log(`app is listening on port ${PORT}`)
