753cff95d584726b3ae71ad25ad0d1031ef5564a
Akhil1sharma <168325399+Akhil1sharma@users.noreply.github.com>
2025-04-03 07:01:55 +0000

    server .js

--- Cook-Book App/backend/server.js
+++ Cook-Book App/backend/server.js
@@ -1,24 +1,19 @@
-const express = require("express");
-const dotenv = require("dotenv").config();
-const connectDb = require("./config/connectionDb");
-const cors = require("cors");
+const express=require("express")
+const app=express()
+const dotenv=require("dotenv").config()
+const connectDb=require("./config/connectionDb")
+const cors=require("cors")
 
-const app = express();
+const PORT=process.env.PORT || 3000
+connectDb()
 
-// Connect to Database
-connectDb();
-
-// Middleware
-app.use(express.json()); // Parse JSON request body
-app.use(cors()); // Enable Cross-Origin Resource Sharing
-app.use(express.static("public")); // Serve static files from the "public" folder
-
+app.use(express.json())
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(express.static("public"))
 
 app.use("/",require("./routes/user"))
 app.use("/recipe",require("./routes/recipe"))
 
-// Server Configuration
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+app.listen(PORT,(err)=>{
+    console.log(`app is listening on port ${PORT}`)
+})
